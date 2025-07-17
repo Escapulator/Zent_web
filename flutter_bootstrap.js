d0790c234845aee0adc4703c91641c447e70d795
escapulator <fadilbaqrey@gmail.com>
2025-07-17 10:48:41 +0100

    Fix web icon

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4186046429"
+    serviceWorkerVersion: "3056615626"
   }
 });
